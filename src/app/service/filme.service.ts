1c913b98b8a44dd444d60c09103f4ebb5f47809e
Marcilio Freitas <marcilio@beltsistemas.com.br>
2023-05-08 17:20:40 -0300

    Final da prova.

--- src/app/service/filme.service.ts
+++ src/app/service/filme.service.ts
@@ -10,32 +10,32 @@ export class FilmeService {
   constructor() {
      this.filmes = [
       {
-        "titulo": "teste",
-        "diretor": "teste",
-        "origem" : "teste",
-        "cartaz" : "teste",
-        "ano": 2020
+        "titulo": "Os Vingadores",
+        "diretor": "Joss Whedon",
+        "origem" : "EUA",
+        "cartaz" : "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQmkXsIZ89RVeyZkI_v3cLLB4MFd-KlVNXub-M16petJKffpZiw",
+        "ano": 2012
       },
       {
-        "titulo": "teste",
-        "diretor": "teste",
-        "origem" : "teste",
-        "cartaz" : "teste",
-        "ano": 2020
+        "titulo": "Os Vingadores - Era de Ultron",
+        "diretor": "Joss Whedon",
+        "origem" : "EUA",
+        "cartaz" : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRD559ar0kYM2Gp3E9xv1MUgwQ-O6n1Ytj8Vtr__EFsGofsuwWu",
+        "ano": 2015
       },
       {
-        "titulo": "teste",
-        "diretor": "teste",
-        "origem" : "teste",
-        "cartaz" : "teste",
-        "ano": 2020
+        "titulo": "Os Vingadores - Guerra Infinita",
+        "diretor": "Anthony Russo, Joe Russo",
+        "origem" : "EUA",
+        "cartaz" : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcS4neC_Y4U1G6u2QlEFqbCizheGBUnZg1w874qWbIcVlv9tPuSu",
+        "ano": 2018
       },
       {
-        "titulo": "teste",
-        "diretor": "teste",
-        "origem" : "teste",
-        "cartaz" : "teste",
-        "ano": 2020
+        "titulo": "Os Vingadores - Ultimato",
+        "diretor": "Anthony Russo, Joe Russo",
+        "origem" : "EUA",
+        "cartaz" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMwtU95JygcmCq4e3XPpL9W0ATjGFoQCQm16dbpIp7BM36B7lH",
+        "ano": 2019
       },
     ]
    }
